d7c8a031a4e9958e8023dffd09334b3dcd5f73d2
Your Name <you@example.com>
2018-06-14 11:33:16 +0200

    Commented

--- Mastermind/DodatkowaPraca/Program.cs
+++ Mastermind/DodatkowaPraca/Program.cs
@@ -10,9 +10,9 @@ namespace DodatkowaPraca
         private static void Main(string[] args)
         {
             var connectionString = ((NameValueCollection) ConfigurationManager.GetSection("ScoreClassConfig"))["ConnectionString"];
-            var scoreManager = new ScoreManager(connectionString, 10);
+            var scoreManager = new ScoreManager(connectionString, 10);      
 
-            var flow = new GameFlow(new MastermindGame(), null);
+            var flow = new GameFlow(new MastermindGame(), null);        // Here u can choose the game
 
             flow.Run();
         }
